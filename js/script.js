7e39af0c836b3a57849440ad8e4abb34dd9b9495
Syazwan0317 <syazwansoleh2018@gmail.com>
2026-06-09 19:43:12 +0800

    Implement mobile menu toggle and typing animation
    Added mobile menu toggle functionality and typing animation effect.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
-const themeBtn = document.getElementById("themeBtn");
+const menuToggle = document.getElementById("menuToggle");
+const navLinks = document.getElementById("navLinks");
 
-if(themeBtn){
-    themeBtn.addEventListener("click", () => {
-        document.body.classList.toggle("dark");
-    });
+/* MOBILE MENU */
+menuToggle.addEventListener("click", () => {
+navLinks.style.display =
+navLinks.style.display === "flex" ? "none" : "flex";
+});
+
+/* TYPING ANIMATION */
+const text = "Software Developer in Progress...";
+let i = 0;
+
+function typeEffect(){
+document.querySelector(".typing").innerHTML =
+text.substring(0, i);
+
+i++;
+
+if(i <= text.length){
+setTimeout(typeEffect, 80);
 }
+}
+
+typeEffect();
